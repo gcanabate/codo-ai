eb86a87e59897d76f0d2042427fd7f3486e78b61
Gaël Canabaté <gael.canabate@gmail.com>
2023-05-12 10:07:35 +0200

    delete console log

--- client/script.js
+++ client/script.js
@@ -88,8 +88,6 @@ const handleSubmit = async (e) => {
     const data = await response.json();
     const parsedData = data.bot.trim();
 
-    console.log({ parsedData });
-
     typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
